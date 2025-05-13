3d6b923cf33d4bb82e8ce6516d34bec3c032a73a
Rodrigo Victor <rodrigo.victor@syniti.com>
2025-05-13 17:32:13 -0300

    feat(harpoon): update to v2

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,16 +1,40 @@
 return {
   {
     "ThePrimeagen/harpoon",
-    lazy = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = true,
-    keys = {
-      { "<leader>hm", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
-      { "<c-m>", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
-      { "<c-e>", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
-      { "<leader>ha", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
-    },
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+
+      harpoon:setup()
+
+      vim.keymap.set("n", "<leader>a", function()
+        harpoon:list():add()
+      end)
+
+      vim.keymap.set("n", "<C-e>", function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+
+      vim.keymap.set("n", "<C-h>", function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set("n", "<C-t>", function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set("n", "<C-n>", function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set("n", "<C-s>", function()
+        harpoon:list():select(4)
+      end)
+
+      vim.keymap.set("n", "<C-S-P>", function()
+        harpoon:list():prev()
+      end)
+      vim.keymap.set("n", "<C-S-N>", function()
+        harpoon:list():next()
+      end)
+    end,
   },
 }
