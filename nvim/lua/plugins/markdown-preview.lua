c3064e3a14c0c7d6abe5bec936e89a8e5e6f797b
Rodrigo Victor <rodrigovictor81@gmail.com>
2025-05-21 07:07:47 -0300

    chore(nvim): remake markdown preview keymap

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -7,7 +7,7 @@ return {
     end,
     keys = {
       { "<leader>fmp", "<cmd>MarkdownPreview<cr>", desc = "Markdown Preview" },
-      { "<leader>fmps", "<cmd>MarkdownPreviewStop<cr>", desc = "Stop Markdown Preview" },
+      { "<leader>fmp", "<cmd>MarkdownPreviewStop<cr>", desc = "Stop Markdown Preview" },
     },
   },
 }
